8a0af61c69eb8d6569624684b081456a0a6f1968
ShahwarFatma <shahwar.fatma_cs19@gla.ac.in>
2020-08-30 22:51:13 +0530

    Add equals() and hashCode() methods to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Student {
     private String nameOfTheStudent;
     private long universityRollNumberOfTheStudent;
@@ -50,5 +52,20 @@ public class Student {
     public String toString() {
         return "Name Of The Student:" + nameOfTheStudent + "University Roll Number Of The Student:" + universityRollNumberOfTheStudent +
                 "Number Of Books Issued By The Student:" + numberOfBooksIssuedByTheStudent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return getUniversityRollNumberOfTheStudent() == student.getUniversityRollNumberOfTheStudent() &&
+                getNumberOfBooksIssuedByTheStudent() == student.getNumberOfBooksIssuedByTheStudent() &&
+                getNameOfTheStudent().equals(student.getNameOfTheStudent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNameOfTheStudent(), getUniversityRollNumberOfTheStudent(), getNumberOfBooksIssuedByTheStudent());
     }
 }
