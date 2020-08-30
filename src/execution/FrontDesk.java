9395c970f72b15137741f7137c32d451d5063cea
ShahwarFatma <shahwar.fatma_cs19@gla.ac.in>
2020-08-30 23:56:49 +0530

    Add Main Menu to FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -10,6 +10,7 @@ import definitions.Book;
 import definitions.Student;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class FrontDesk {
     public static void main(String[] args) {
@@ -28,5 +29,43 @@ public class FrontDesk {
         student.setUniversityRollNumberOfTheStudent(19040031);
         student.setNumberOfBooksIssuedByTheStudent(1);
         System.out.println(student.toString());
+
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("-=-=--=-=-Welcome To The Front Desk-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Please Enter your choice between 1 to 4: ");
+            int studentChoice = scanner.nextInt();
+            switch (studentChoice) {
+                case 1: {
+                    System.out.println("Please enter your name: ");
+                    String nameOfTheStudent (scanner.nextLine());
+                    System.out.println("Please enter your University roll number: ");
+                    long universityRollNumberOfTheStudent (scanner.nextLong());
+                    System.out.println("Which book do you want to issue?");
+                    String nameOfTheBook (scanner.nextLine());
+                    System.out.println("Thank you for Issuing the Book.");
+                    break;
+                }
+                case 2:
+                    System.out.println("Please enter the name of the book you want to return.");
+                    String bookName (scanner.nextLine());
+                    System.out.println("Thank you for Returning the Book.");
+                    break;
+                case 3:
+                    System.out.println("Here are the books issued by you.");
+                default:
+                    System.out.println("Sorry! You have entered wrong choice");
+                    break;
+            }
+
+            while (studentChoice != 4) ;
+            scanner.close();
+        }
+
     }
 }
